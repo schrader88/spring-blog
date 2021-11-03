08c1690fbf0c4b9f2f97da282e57b3135b1234b1
Kyle Schrader <kyle.schrader97@gmail.com>
2021-11-03 11:05:10 -0500

    finished all Views exercises. final commit and push

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -1,24 +1,40 @@
 package com.codeup.springblog.controllers;
 
+import com.codeup.springblog.models.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class PostController {
 
     @GetMapping("/posts")
-    @ResponseBody
-    public String showPosts() {
-        return "Showing all posts:";
+    public String showPosts(Model model) {
+        Post postOne = new Post("My First Blog", "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid animi autem consectetur culpa cumque doloribus eaque esse facilis, fugit ipsum laudantium minima mollitia, possimus quidem rem repellendus soluta tenetur voluptatem!");
+        Post postTwo = new Post("My Second Blog", "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid animi autem consectetur culpa cumque doloribus eaque esse facilis, fugit ipsum laudantium minima mollitia, possimus quidem rem repellendus soluta tenetur voluptatem!");
+
+        List<Post> posts = new ArrayList<>();
+
+        posts.add(postOne);
+        posts.add(postTwo);
+
+        model.addAttribute("posts", posts);
+
+        return "posts/index";
     }
 
     @GetMapping("/posts/{id}")
-    @ResponseBody
-    public String showSinglePost(@PathVariable long id) {
-        return "Showing all posts: " + id;
+    public String showSinglePost(@PathVariable long id, Model model) {
+        Post post = new Post("My First Blog", "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid animi autem consectetur culpa cumque doloribus eaque esse facilis, fugit ipsum laudantium minima mollitia, possimus quidem rem repellendus soluta tenetur voluptatem!");
+
+        model.addAttribute("post", post);
+        return "posts/show";
     }
 
     @GetMapping("/posts/create")
