70c310e9948d8cd1a80265051f902fb79a01c708
Kyle Schrader <kyle.schrader97@gmail.com>
2021-11-03 12:30:00 -0500

    about halfway through JPA mini exercise

---
+++ src/main/java/com/codeup/springblog/models/Dog.java
@@ -0,0 +1,62 @@
+package com.codeup.springblog.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "dogs")
+public class Dog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(columnDefinition = "TINYINT(3) UNSIGNED NOT NULL")
+    private int age;
+
+    @Column(columnDefinition = "VARCHAR(200) NOT NULL")
+    private String name;
+
+    @Column(columnDefinition = "CHAR(2) DEFAULT 'XX'")
+    private String resideState;
+
+    public Dog() {
+    }
+
+    public Dog(long id, int age, String name, String resideState) {
+        this.id = id;
+        this.age = age;
+        this.name = name;
+        this.resideState = resideState;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getResideState() {
+        return resideState;
+    }
+
+    public void setResideState(String resideState) {
+        this.resideState = resideState;
+    }
+}
