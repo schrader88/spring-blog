8b346d183b7e85f8a82ef548ed495fd050c11a26
Kyle Schrader <kyle.schrader97@gmail.com>
2021-11-09 12:13:24 -0600

    finished housekeeping. added Create an Post to navbar and redirected landing page

--- src/main/java/com/codeup/springblog/controllers/HomeController.java
+++ src/main/java/com/codeup/springblog/controllers/HomeController.java
@@ -8,9 +8,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class HomeController {
 
     @GetMapping("/")
-    @ResponseBody
     public String returnLandingPage() {
-        return "This is the landing page!";
+        return "redirect:/posts";
     }
 
 }
