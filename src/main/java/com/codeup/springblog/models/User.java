2565ce88e3eef0f9966337ecbf15d562be649096
Kyle Schrader <kyle.schrader97@gmail.com>
2021-11-09 11:50:34 -0600

    finished Spring Security exercises

--- src/main/java/com/codeup/springblog/models/User.java
+++ src/main/java/com/codeup/springblog/models/User.java
@@ -12,10 +12,10 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String username;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String email;
 
     @Column(nullable = false)
@@ -35,6 +35,13 @@ public class User {
         this.username = username;
         this.email = email;
         this.password = password;
+    }
+
+    public User(User copy) {
+        id = copy.id; // This line is SUPER important! Many things won't work if it's absent
+        email = copy.email;
+        username = copy.username;
+        password = copy.password;
     }
 
     public long getId() {
